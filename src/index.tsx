057ed4af00db9f73ca1ff3e7675c94b35af17c83
codiechanel <codiechanel@gmail.com>
2019-12-25 18:13:46 +0800

    d

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Button } from 'react-bootstrap';
 
 function App(props) {
 	console.log(process.env.cool);
-	return <div>Helloh jk wow jj</div>;
+	return (
+		<div>
+			Helloh jk jj
+			<Button>cool</Button>
+		</div>
+	);
 }
 
 ReactDOM.render(
